952b67806217204826fe864b784f3a0dd7748283
aboubakrinea <aboubakrineabderrahmane@gmail.com>
2026-05-04 02:04:39 +0100

    initialisation du projet

---
+++ src/test/java/com/example/gestionProduit/GestionProduitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionProduit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProduitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
