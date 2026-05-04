2529301327c47d9dad100c8482e824514faa8888
aboubakrinea <aboubakrineabderrahmane@gmail.com>
2026-05-04 02:18:28 +0100

    creation dune Entity

---
+++ src/main/java/com/example/gestionProduit/entity/Product.java
@@ -0,0 +1,14 @@
+package com.example.gestionProduit.entity;
+import jakarta.persistence.*;
+import lombok.Data;
+@Entity
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    
+}
