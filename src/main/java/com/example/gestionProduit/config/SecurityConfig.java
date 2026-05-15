0a0eaffbd8bfe9c751c0572754b6b8af961a41b6
aboubakrinea <aboubakrineabderrahmane@gmail.com>
2026-05-15 20:19:22 +0100

    modifier Secrurite configuratin

--- src/main/java/com/example/gestionProduit/config/SecurityConfig.java
+++ src/main/java/com/example/gestionProduit/config/SecurityConfig.java
@@ -1,23 +1,19 @@
 package com.example.gestionProduit.config;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
-@EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/**").permitAll() // Permet l'accès à toutes les routes de l'API
-                .anyRequest().authenticated() // Exige une authentification pour les autres routes
-            )   
-            .httpBasic(null); // Utilise l'authentification HTTP Basic
+                .anyRequest().permitAll()
+            );
         return http.build();
     }
 }
