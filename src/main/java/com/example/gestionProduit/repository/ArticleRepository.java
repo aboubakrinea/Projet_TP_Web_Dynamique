22fda480c028882787ee7bcb594361971bda7463
aboubakrinea <aboubakrineabderrahmane@gmail.com>
2026-06-15 10:34:44 +0100

    ArticleRepository

---
+++ src/main/java/com/example/gestionProduit/repository/ArticleRepository.java
@@ -0,0 +1,7 @@
+package com.example.gestionProduit.repository;
+import com.example.gestionProduit.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+}
